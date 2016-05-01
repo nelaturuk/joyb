ba92ec130536a49d1389502d462f14d06fa743fc
Keerthi Nelaturu <nelaturuk@gmail.com>
2016-05-01 11:15:15 -0400

    Changes to hibernate

--- dao/src/main/java/hibernate/HibernateEntityInterceptor.java
+++ dao/src/main/java/hibernate/HibernateEntityInterceptor.java
@@ -1,6 +1,10 @@
 package hibernate;
 
+import java.io.Serializable;
+import java.util.Iterator;
+
 import org.hibernate.EmptyInterceptor;
+import org.hibernate.type.Type;
 
 public class HibernateEntityInterceptor extends EmptyInterceptor {
 
@@ -8,7 +12,39 @@ public class HibernateEntityInterceptor extends EmptyInterceptor {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	
+
+	public void onDelete(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
+
+		// log delete events
+		System.out.println("Delete event");
+	}
+
+	// called when a Student gets updated.
+	public boolean onFlushDirty(Object entity, Serializable id, Object[] currentState, Object[] previousState,
+			String[] propertyNames, Type[] types) {
+		return false;
+	}
+
+	// called on load events
+	public boolean onLoad(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
+
+		// log loading events
+		System.out.println("Load Operation");
+		return true;
+	}
+
+	public boolean onSave(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
+		return false;
+	}
+
+	// called before commit into database
+	public void preFlush(Iterator iterator) {
+		System.out.println("Before commiting");
+	}
+
+	// called after committed into database
+	public void postFlush(Iterator iterator) {
+		System.out.println("After commiting");
+	}
 
 }
