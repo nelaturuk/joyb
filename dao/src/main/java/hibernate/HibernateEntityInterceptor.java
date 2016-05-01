ada355d6d846028749623eb138d4b2695be09634
Keerthi Nelaturu <nelaturuk@gmail.com>
2016-05-01 11:08:17 -0400

    Changes to add hibernte

---
+++ dao/src/main/java/hibernate/HibernateEntityInterceptor.java
@@ -0,0 +1,14 @@
+package hibernate;
+
+import org.hibernate.EmptyInterceptor;
+
+public class HibernateEntityInterceptor extends EmptyInterceptor {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}
